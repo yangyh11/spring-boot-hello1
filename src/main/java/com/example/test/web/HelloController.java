2e9660716099300357b527b0f6f3ea205ebf76b0
yangyh <yangyh0609@cairenhui.com>
2019-02-25 11:59:50 +0800

    springboot起步之hello world，单纯的启动类

---
+++ src/main/java/com/example/test/web/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.test.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description:
+ * @author: yangyh
+ * @create: 2019-02-25 11:32
+ **/
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String hello(){
+        return "hello world";
+    }
+}
--- src/test/java/com/example/SpringBootHello1ApplicationTests.java
+++ src/test/java/com/example/SpringBootHello1ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.springboothello1;
+package com.example;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
