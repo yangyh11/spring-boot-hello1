31d2267ac587365c19930dbf6616c0db6ffc4f24
yangyh <yangyh0609@cairenhui.com>
2019-02-27 13:37:23 +0800

    spring-boot:使用servlet

---
+++ src/main/java/com/example/base/servlet/MyServlet.java
@@ -0,0 +1,39 @@
+package com.example.base.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @description:
+ * @author: yangyh
+ * @create: 2019-02-27 13:30
+ **/
+@WebServlet("/myServlet/*")
+public class MyServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println(">>>>>>>>>>doGet()<<<<<<<<<<<");
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println(">>>>>>>>>>doPost()<<<<<<<<<<<");
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Hello World</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>myServlet</h1>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
